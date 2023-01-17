aca80a39a1bae5135e3b85f11639648f85c2b381
Eduard <dirol2016@list.ru>
2023-01-17 19:29:39 +0500

    DZ_2

---
+++ DZ_2/Notebook.java
@@ -0,0 +1,74 @@
+// Задание:
+// Класс Notebook содержит поля:
+// Стоимость - price, double [100, 200, 300, 400, 500, 600, 700, 800]
+// Оперативная память - ram, integer [4, 8, 16, 20, 24, 28, 32]
+// Производитель - brand, enum Перечисление Brand может принимать следующие значения: Lenuvo, Asos, MacNote, Eser, Xamiou.
+// Сортировать нужно в этом же порядке (т.е. Lenuvo имеет наивысший приоритет).
+// Отсортировать по стоимости, по памяти, по перечислению (в таком порядке)
+
+package DZ_2;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Notebook implements Comparable<Notebook> {
+    enum Brand {
+        LENUVO("Lenuvo"),
+        ASOS("Asos"),
+        MACNOTE("MacNote"),
+        ESER("Eser"),
+        XAMIOU("Xamiou");
+        private String title;
+        Brand(String title) {
+            this.title = title;
+        }
+        public String getTitle() {
+            return this.title;
+        }
+        @Override
+        public String toString() {
+            return this.title;
+        }
+    };
+    }
+
+    ;
+
+    private static double[] PRICE_VALUES = {100, 200, 300, 400, 500, 600, 700, 800};
+    private static int[] RAM_VALUES = {4, 8, 16, 20, 24, 28, 32};
+    private static Brand[] BRAND_VALUES = Brand.values();
+
+    private double price;
+    private int ram;
+    private Brand brand;
+
+
+    public Notebook() {
+        Random rnd = new Random();
+        price = PRICE_VALUES[rnd.nextInt(PRICE_VALUES.length)];
+        ram = RAM_VALUES[rnd.nextInt(RAM_VALUES.length)];
+        Brand[] brands = Brand.values();
+        brand = brands[rnd.nextInt(brands.length)];
+        brand = BRAND_VALUES[rnd.nextInt(BRAND_VALUES.length)];
+    }
+
+    @Override
+    public String toString() {
+        return "Notebook{"
+                + "price=" + price
+                + " ram=" + ram
+                + " brand=" + brand
+                + '}';
+    }
+    @Override
+    public int compareTo(Notebook o) {
+        int compare = Double.compare(this.price, o.price);
+        if (compare != 0)
+            return compare;
+        compare = Integer.compare(this.ram, o.ram);
+        if (compare != 0)
+            return compare;
+        return Integer.compare(this.brand.ordinal(), o.brand.ordinal());
+    }
+}
+
